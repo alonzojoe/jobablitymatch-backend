790fa195d640a10d8bb0c09a30eaa603e4e03c1e
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-04-24 15:33:30 +0800

    feat: create model controller for Role includes CRUD method + Route API registration

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-
+use App\Http\Controllers\API\RoleController;
 
 Route::get('/test', function () {
     return response()->json(['status' => 'success', 'message' => 'API Endpoint Works!'], 200);
@@ -18,4 +18,11 @@ Route::group(['prefix' => '/auth'], function () {
     Route::post('/me', [AuthController::class, 'me']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::post('/change-password', [AuthController::class, 'changePassword']);
+});
+
+Route::group(['prefix' => '/role'], function () {
+    Route::get('/', [RoleController::class, 'index']);
+    Route::post('/store', [RoleController::class, 'store']);
+    Route::patch('/update/{id}', [RoleController::class, 'update']);
+    Route::patch('/destroy/{id}', [RoleController::class, 'destroy']);
 });
