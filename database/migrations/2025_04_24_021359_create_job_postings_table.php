9a72a932a8e4e3a1db2af2a5850f964b99d4e8a0
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-04-24 10:46:30 +0800

    feat: create  schema for job_postings table

---
+++ database/migrations/2025_04_24_021359_create_job_postings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_postings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->longText('description')->nullable();
+            $table->bigInteger('company_id')->nullable();
+            $table->boolean('active')->default(1);
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_postings');
+    }
+};
