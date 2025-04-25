1c7581bd9e4a351407808323252c281eadc42e1e
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-04-25 11:03:37 +0800

    feat: create new model controller for DisabilityType + API route registration

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\RoleController;
 use App\Http\Controllers\API\CompanyController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\DisabilityTypeController;
+
 
 Route::get('/test', function () {
     return response()->json(['status' => 'success', 'message' => 'API Endpoint Works!'], 200);
@@ -24,7 +26,7 @@ Route::group(['prefix' => '/auth'], function () {
 
 Route::group(['prefix' => '/role'], function () {
     Route::get('/', [RoleController::class, 'index']);
-    Route::post('/store', [RoleController::class, 'store']);
+    Route::post('/create', [RoleController::class, 'store']);
     Route::patch('/update/{id}', [RoleController::class, 'update']);
     Route::patch('/destroy/{id}', [RoleController::class, 'destroy']);
 });
@@ -32,7 +34,7 @@ Route::group(['prefix' => '/role'], function () {
 
 Route::group(['prefix' => '/company'], function () {
     Route::get('/', [CompanyController::class, 'index']);
-    Route::post('/store', [CompanyController::class, 'store']);
+    Route::post('/create', [CompanyController::class, 'store']);
     Route::patch('/update/{id}', [CompanyController::class, 'update']);
     Route::patch('/destroy/{id}', [CompanyController::class, 'destroy']);
 });
@@ -43,4 +45,12 @@ Route::group(['prefix' => '/user'], function () {
     Route::get('/{id}', [UserController::class, 'show']);
     Route::put('/update/{id}', [UserController::class, 'update']);
     Route::patch('/destroy/{id}', [UserController::class, 'destroy']);
+});
+
+Route::group(['prefix' => '/disability'], function () {
+    Route::get('/', [DisabilityTypeController::class, 'index']);
+    Route::get('/{id}', [DisabilityTypeController::class, 'show']);
+    Route::post('/create', [DisabilityTypeController::class, 'store']);
+    Route::put('/update/{id}', [DisabilityTypeController::class, 'update']);
+    Route::patch('/destroy/{id}', [DisabilityTypeController::class, 'destroy']);
 });
