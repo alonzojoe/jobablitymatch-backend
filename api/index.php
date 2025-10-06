e3bad88795051857556d132d46fb6e9e6a03d2d2
alonzojoe <alonzojoenell2k15@gmail.com>
2025-10-06 19:03:16 +0800

    update vercel config

--- api/index.php
+++ api/index.php
@@ -2,6 +2,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Fix the request URI so Laravel sees the correct route
+$_SERVER['REQUEST_URI'] = '/' . ltrim($_SERVER['PATH_INFO'] ?? '', '/');
+
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
