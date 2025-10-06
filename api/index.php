c8f4a7917962e9b96629b297d754649467dd268b
alonzojoe <alonzojoenell2k15@gmail.com>
2025-10-06 15:49:27 +0800

    Update Vercel config

--- api/index.php
+++ api/index.php
@@ -1,12 +1,14 @@
 <?php
-require __DIR__ . '/../public/index.php';
-// require __DIR__ . '/../vendor/autoload.php';
 
-// $app = require_once __DIR__ . '/../bootstrap/app.php';
+// Load Laravel App
 
-// $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-// $response = $kernel->handle(
-//     $request = Illuminate\Http\Request::capture()
-// );
-// $response->send();
-// $kernel->terminate($request, $response);
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
