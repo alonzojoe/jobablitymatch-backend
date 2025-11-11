a658e495bb1d3a79dadf31e54269c1f66524589e
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-11 11:31:08 +0800

    create new table migration to modify job postings table add new columns hiring_from and hiring_to and include in Model $fillable array

---
+++ database/migrations/2025_11_11_031825_modify_job_postings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('job_postings', function (Blueprint $table) {
+            $table->date('hiring_from')->nullable()->after('status');
+            $table->date('hiring_to')->nullable()->after('hiring_from');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('job_postings', function (Blueprint $table) {
+            $table->dropColumn(['hiring_from', 'hiring_to']);
+        });
+    }
+};
